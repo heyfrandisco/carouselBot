2221d174eb309321f8890460b07c3c4eba4e4162
Francisco <77542565+2inthemorningg@users.noreply.github.com>
2022-04-10 21:38:32 +0100

    Update carouselbot.py

--- carouselbot.py
+++ carouselbot.py
@@ -3,7 +3,7 @@
 # Tested by atlasjx, FPort0
 
 # Note: The sleep delay exists to prevent an "overload" that would cause the movement to glitch. 
-# You can play with the time frame to find the optimum balance of being annoying and not destroying the app
+# You can play with the time frame to find the optimum balance of being annoying and not destroying the app.
 # Have fun :)
 
 import discord
