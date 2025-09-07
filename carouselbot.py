db7b5d8af54b5459dcd04c4ef60e946bbb5be45e
Francisco <77542565+heyfrandisco@users.noreply.github.com>
2025-09-07 12:29:24 +0100

    Update carouselbot.py

--- carouselbot.py
+++ carouselbot.py
@@ -1,5 +1,5 @@
-# Created by fcallanam
-# Developed by fcallanam
+# Created by heyfrandisco
+# Developed by heyfrandisco
 
 # Note: The sleep delay exists to prevent an "overload" that would cause the movement to glitch. 
 # You can play with the time frame to find the optimum balance between being annoying and not destroying the app.
@@ -16,7 +16,7 @@ TOKEN = {token}
 @bot.event
 async def on_ready():
     print("Logged in as {0.user}".format(bot))
-    print("Bot made by fcallanam")
+    print("Bot made by heyfrandisco")
     print("Version 1.0.0")
 
 
